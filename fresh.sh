03514c5d6c1a427c8ea7b30fd6a9d5e693362ca4
mtshv <i.mityushev@gmail.com>
2024-10-02 22:21:41 +0300

    Modify fresh.sh

--- fresh.sh
+++ fresh.sh
@@ -26,19 +26,15 @@ brew update
 brew tap homebrew/bundle
 brew bundle --file ./Brewfile
 
-# Set default MySQL root password and auth type
-mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY 'password'; FLUSH PRIVILEGES;"
-
 # Create a projects directories
-mkdir $HOME/Code
-mkdir $HOME/Herd
+mkdir $HOME/dev
 
-# Create Code subdirectories
-mkdir $HOME/Code/blade-ui-kit
-mkdir $HOME/Code/laravel
+## Create Code subdirectories
+mkdir $HOME/dev/work
+mkdir $HOME/dev/personal
 
-# Clone Github repositories
-./clone.sh
+## Clone Github repositories
+#./clone.sh
 
 # Symlink the Mackup config file to the home directory
 ln -s ./.mackup.cfg $HOME/.mackup.cfg
